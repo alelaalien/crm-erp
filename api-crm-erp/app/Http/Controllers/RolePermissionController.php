f8df6d85473763945e9fde2631f6c608a0746614
Ale <alelaalien@gmail.com>
2026-05-13 20:12:50 +0200

    toastr added

--- api-crm-erp/app/Http/Controllers/RolePermissionController.php
+++ api-crm-erp/app/Http/Controllers/RolePermissionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\DB;
 
 class RolePermissionController extends Controller
 {
@@ -13,22 +14,34 @@ class RolePermissionController extends Controller
     
 public function index(Request $request)
 {
-    $search = $request->get("search");
 
-    $roles = Role::where("name", "like", "%".$search."%")
-        ->orderBy("id", "desc")
-        ->paginate(25);
+    try {
+            $search = $request->get("search");
 
-    return response()->json([
-        "total" => $roles->total(),
-        "roles" => $roles->map(function($rol){
-            
-             $rol->permission_pluck = $rol->permissions->pluck("name");
-             $rol->updated_format_at = $rol->updated_format_at->format("d-m-Y h:i A");
+            $roles = Role::where("name", "like", "%".$search."%")
+                ->orderBy("id", "desc")
+                ->paginate(25);
 
-             return $rol;
-        })
-    ]);
+            return response()->json([
+                "status" => "success",
+                "total" => $roles->total(),
+                "roles" => $roles->map(function($rol){
+                    
+                    $rol->permission_pluck = $rol->permissions->pluck("name");
+                    $rol->updated_format_at = $rol->updated_at ? $rol->updated_at->format("d-m-Y h:i A") : null;
+
+                    return $rol; 
+                })      
+                ], 200);  
+    } catch (\Exception $e) {
+        return response()->json([
+                "status" => "error",
+                "message" => "Something went wrong.",
+                "debug" => $e->getMessage()  
+            ], 500);
+    }
+    
+   
 }
 
     /**
@@ -36,41 +49,57 @@ public function index(Request $request)
      */
     public function store(Request $request)
     {
-        $isRole = Role::where("name", $request->name)->first();
-        
-        if($isRole)
-            {
-                return response()->json(
-                    [
-                        "code" => 403,
-                        "message" => "Item already exists"
-                    ]
-                );
-            }
 
-            $role= Role::create([
+    try {
+        $request->validate([
+                'name'=>'required|unique:roles,name',
+                'permissions' => 'required|array'
+        ]);
 
+        $role = DB::transaction(function() use($request){
+
+            $newRole = Role::create([
                 'guard_name' => 'api',
                 'name' => $request->name
             ]);
+
+            $newRole->syncPermissions($request->permissions);
+
+            return $newRole;
+       });
+
+       return response()->json([
+
+        'status'=>'success',
+        "message" => "Role created",
+        "role"=>[
+                    "id" => $role->id,
+                    "permission" => $role->permissions,
+                    "permission_pluck"  => $role->permissions->pluck("name"),
+                    "updated_at" => $role->updated_at ? $role->updated_at->format('d-m-Y h:i A') : null,
+                    "name" => $role->name
+                ]
             
-            $role->syncPermissions($request->permissions);
-            /*          foreach ($request->permissions as $key => $permission) {
-                $role-> givePermissionTo($permission);
-            }
-            */
+       ]);
+    } catch (\Illuminate\Validation\ValidationException $e) {
+         
+        return response()->json([
+            "status"=> "error",
+            "message" => "invalid data",
+            "errors" => $e->errors()
+        ], 422);
+    
+    } catch(\Exception $e){
 
-            return response()->json([
-
-                "code"=> 200,
-                "message"=>[
-                     "id" => $role->id,
-                     "permission" => $role->permissions,
-                     "permission_pluck"  => $role->permissions->pluck("name"),
-                     "updated_at" => $role->updated_format_at->format('d-m-Y h:i A'),
-                     "name" => $role->name
-                     ]
-            ]); 
+        return response()->json([
+            "status" => "error",
+          //  "debug" => $e->getMessage(),
+            "message" => "Something went wrong while role creating",
+            "line" => $e->getLine(),
+            "file" => $e->getFile()
+        ], 500);
+    }
+ 
          
     }
 
